86aceacb5efc1006f1009fc0e655c63ebdff3b58
Money Mafia <97112807+moneymafia@users.noreply.github.com>
2022-01-05 13:58:09 +0530

    ---

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,34 @@
 <template>
-  <div id="app">HELLO WORLD</div>
+  <div id="app">HELLO WORLD
+  
+  
+  
+  
+  
+  <footer class="py-20">
+  <div class="container px-4 mx-auto text-center">
+
+    <ul class="my-6 flex flex-wrap space-x-8 items-center justify-center">
+      <li class="mb-2 md:mb-0"><a class="text-sm text-gray-900 hover:text-gray-700" href="#">About</a></li>
+      <li class="mb-2 md:mb-0"><a class="text-sm text-gray-900 hover:text-gray-700" href="#">Company</a></li>
+      <li class="mb-2 md:mb-0"><a class="text-sm text-gray-900 hover:text-gray-700" href="#">Services</a></li>
+      <li class="mb-2 md:mb-0"><a class="text-sm text-gray-900 hover:text-gray-700" href="#">Testimonials</a></li>
+    </ul>
+  </div>
+  <div class="mt-12 mb-8 border-b border-gray-50"></div>
+  <div class="container px-4 mx-auto">
+    <p class="text-center text-sm text-gray-400">All rights reserved &copy; Wireframes Corporation 2020</p>
+  </div>
+</footer>
+  
+  
+  
+  
+  
+  </div>
+
+
+
 </template>
 
 <script>
