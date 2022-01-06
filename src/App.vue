9d2f35f05cf059c4c29f1bfc76403b9d6a24d454
Money Mafia Manager <97112807+MoneyMafiaManager@users.noreply.github.com>
2022-01-06 18:29:25 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
 	<div id="app">
-		HELLO WORLD
+	<section class="py-5 bg-white ">
+		<div class="container flex flex-col flex-wrap items-center justify-between py-5 mx-auto md:flex-row ">
+			<div class="relative flex flex-col md:flex-row">
+				<router-link to="/" class="flex items-center mb-5 font-medium text-gray-900 lg:w-auto lg:items-center lg:justify-center md:mb-0">
+					<span class="mx-auto text-xl font-black leading-none text-gray-900 select-none">MONEY MAFIA</span>
+				</router-link>
+			</div>
+
+			<div class="inline-flex items-center ml-5 space-x-6 lg:justify-end">
+				<nav class="flex flex-wrap items-center mb-5 text-base md:mb-0 md:pl-8 md:ml-8 md:border-l md:border-gray-200">
+					<router-link to="/service" class="mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900 uppercase">Services</router-link>
+					<router-link to="/faq" class="mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900 uppercase">FAQ</router-link>
+					<router-link to="/team" class="mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900 uppercase">TEAM</router-link>
+					<router-link to="/contact" class="mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900 uppercase">CONTACT</router-link>
+				</nav>
+			</div>
+		</div>
+	</section>
+
 
 		<footer class="py-20">
 			<div class="container px-4 mx-auto text-center">
