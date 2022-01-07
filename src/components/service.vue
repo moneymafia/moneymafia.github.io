a5038bca9c3b4e5f0b316d07240218b31f1aff4b
Money Mafia Manager <97112807+MoneyMafiaManager@users.noreply.github.com>
2022-01-07 12:57:25 +0530

    Add files via upload

---
+++ src/components/service.vue
@@ -0,0 +1,59 @@
+<template>
+	<div>
+		<div class="grid grid-cols-1 md:grid-cols-2 md:gap-8">
+			<div class="col-span-1 m-auto">
+				<h2 class="mb-4 text-3xl font-bold leading-tight tracking-tight sm:text-4xl font-heading"><span class="text-white bg-red-400">DECENTRALISED</span> HEDGE FUND</h2>
+				<p class="mb-4 text-xl tracking-tight">We invest in various early-stage crypto projects we work with these companies to unleash their full potential. The profits are distributed among our token holders through a token buyback.</p>
+			</div>
+			<div class="col-span-1">
+				<lottie-player class="object-cover object-center w-full h-full" autoplay loop mode="normal" src="https://ipfs.infura.io/ipfs/Qmf8FYZSiN24jkZkj5pY54DLgBGXYUPJoEU2fmaPE6P5Sb"> </lottie-player>
+			</div>
+		</div>
+
+		<section class="mx-auto p-10 md:py-20 px-0 md:p-10 md:px-0">
+			<section class="p-5 md:p-0 grid grid-cols-1 md:grid-cols-3 gap-5 items-start">
+				<article class="relative shadow-2xl transform duration-500 hover:-translate-y-2 cursor-pointer rounded-md">
+					<div class="p-8 mb-10 mt-2">
+						<h2 class="text-3xl mt-2">Project Security</h2>
+						<p class="text-xbase mt-5 text-gray-400 leading-relaxed">Get a comprehensive understanding of your security landscape and be confident in your technology.</p>
+					</div>
+				</article>
+
+				<article class="relative shadow-2xl transform duration-500 hover:-translate-y-2 cursor-pointer rounded-md">
+					<div class="p-8 mb-10 mt-2">
+						<h2 class="text-3xl mt-2">Project Optimization</h2>
+						<p class="text-xbase mt-5 text-gray-400 leading-relaxed">We assist projects to make their business logic more effective & scalable based on real world conditions.</p>
+					</div>
+				</article>
+
+				<article class="relative shadow-2xl transform duration-500 hover:-translate-y-2 cursor-pointer rounded-md">
+					<div class="p-8 mb-10 mt-2">
+						<h2 class="text-3xl mt-2">Venture Capital</h2>
+						<p class="text-xbase mt-5 text-gray-400 leading-relaxed">We fund early-stage software startups. Focusing on blockchain startups and web internet based companies.</p>
+					</div>
+				</article>
+
+				<article class="relative shadow-2xl transform duration-500 hover:-translate-y-2 cursor-pointer rounded-md">
+					<div class="p-8 mb-10 mt-2">
+						<h2 class="text-3xl mt-2">Mentorship</h2>
+						<p class="text-xbase mt-5 text-gray-400 leading-relaxed">We are mentoring new projects them and ensuring that they tackle every problem.</p>
+					</div>
+				</article>
+
+				<article class="relative shadow-2xl transform duration-500 hover:-translate-y-2 cursor-pointer rounded-md">
+					<div class="p-8 mb-10 mt-2">
+						<h2 class="text-3xl mt-2">Project Development</h2>
+						<p class="text-xbase mt-5 text-gray-400 leading-relaxed">Team of expert developers, we are always building projects that solve problems.</p>
+					</div>
+				</article>
+
+				<article class="relative shadow-2xl transform duration-500 hover:-translate-y-2 cursor-pointer rounded-md">
+					<div class="p-8 mb-10 mt-2">
+						<h2 class="text-3xl mt-2">Project Audit</h2>
+						<p class="text-xbase mt-5 text-gray-400 leading-relaxed">We are an industry leader in security research. Find your bugs before hackers.</p>
+					</div>
+				</article>
+			</section>
+		</section>
+	</div>
+</template>
