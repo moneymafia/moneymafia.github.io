63d2b16d6dd15d0dfdd9199df21c4be6a286910d
Aman Singh <besoeasy@users.noreply.github.com>
2022-01-22 12:36:35 +0530

    pushed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,6 @@
 	<div>
 		<div class="py-16 mx-auto">
 			<div class="flex md:flex-row flex-col items-center">
-			
 				<div class="md:w-1/2 p-5">
 					<p class="text-3xl font-bold uppercase text-gray-400">Money Mafia is a decenterlised organisation that <span class="text-white bg-red-400">develops</span>, <span class="text-white bg-red-400">manages</span> & <span class="text-white bg-red-400">funds</span> blockchain startups.</p>
 
@@ -24,25 +23,26 @@
 					</div>
 				</div>
 
-					<div class="md:w-1/2 p-5">
+				<div class="md:w-1/2 p-5">
 					<lottie-player class="object-cover object-center" autoplay="" loop="" mode="normal" src="img/main.json" background="transparent"></lottie-player>
 				</div>
-
-
 			</div>
 		</div>
-		<div class="py-10 mx-auto">
-			<div class="grid grid-cols-1 md:grid-cols-2 md:gap-8">
-				<div class="col-span-1 m-auto">
-					<h2 class="mb-4 text-3xl font-bold sm:text-4xl font-heading uppercase">PROJECT <span class="text-white bg-red-400">Development</span></h2>
-					<p class="mb-4 text-xl">We develop early-stage blockchain projects, mainly focusing on projects that deliver a service. Unleash your vision with our mentorship and development expertise.</p>
-				</div>
 
-				<div class="col-span-1 m-auto">
-					<h2 class="mb-4 text-3xl font-bold sm:text-4xl font-heading uppercase">PROJECT <span class="text-white bg-red-400">Investment</span></h2>
-					<p class="mb-4 text-xl">We fund early-stage software startups, mainly focusing on projects based on blockchain and fintech. Let us cover the expenses while you work on your million-dollar idea.</p>
-				</div>
-			</div>
-		</div>
+		<feature />
+
+		<portfolio />
 	</div>
 </template>
+
+<script>
+	import feature from '@/components/feature.vue';
+	import portfolio from '@/components/portfolio.vue';
+
+	export default {
+		components: {
+			feature,
+			portfolio,
+		},
+	};
+</script>
