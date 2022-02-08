ee96581fc24f2b635a6e79df38480c0dec329c0a
Money Mafia Manager <97112807+MoneyMafiaManager@users.noreply.github.com>
2022-02-08 16:01:42 +0530

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
 
 					<div class="py-3 items-center mx-auto space-y-6 text-center uppercase">
 						<div class="relative flex flex-col md:flex-row md:space-x-4">
-							<a href="https://medium.com/@moneymafia" class="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-red-400 rounded-md md:mb-0 hover:bg-red-700 md:w-auto">
+							<a href="https://moneymafia.medium.com/" class="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-red-400 rounded-md md:mb-0 hover:bg-red-700 md:w-auto">
 								Medium
 								<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
 									<path d="M5 12h14M12 5l7 7-7 7"></path>
