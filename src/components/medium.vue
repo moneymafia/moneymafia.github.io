3f53458e3b736d990d49e3b6d276598d92275dd4
Money Mafia Manager <97112807+MoneyMafiaManager@users.noreply.github.com>
2022-02-15 21:35:53 +0530

    Update medium.vue

--- src/components/medium.vue
+++ src/components/medium.vue
@@ -21,13 +21,13 @@
 					</div>
 				</article></a
 			>
-			<a v-bind:href="posts[0].link" target="_blank">
+			<a v-bind:href="posts[2].link" target="_blank">
 				<article class="mx-auto pb-2 transform duration-500 hover:-translate-y-1 cursor-pointer group">
 					<div class="min-h-64 overflow-hidden">
-						<img class="transform duration-300 group-hover:scale-110 h-64" v-bind:src="posts[0].thumbnail" />
+						<img class="transform duration-300 group-hover:scale-110 h-64" v-bind:src="posts[2].thumbnail" />
 					</div>
 					<div class="flex justify-between my-5">
-						<div class="text-base font-semibold">{{ posts[0].title }}</div>
+						<div class="text-base font-semibold">{{ posts[2].title }}</div>
 					</div>
 				</article></a
 			>
