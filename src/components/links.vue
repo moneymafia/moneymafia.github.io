a816cec699a296985d51fe7559375584bf1bf896
besoeasy <besoeasy@users.noreply.github.com>
2022-03-17 23:38:36 +0530

    TG

--- src/components/links.vue
+++ src/components/links.vue
@@ -13,8 +13,8 @@
 					<path d="M5 12h14M12 5l7 7-7 7"></path>
 				</svg>
 			</a>
-			<a :href="invite" class="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-gray-400 rounded-md md:mb-0 hover:bg-gray-700 md:w-auto">
-				Discord
+			<a :href="telegram" class="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-gray-400 rounded-md md:mb-0 hover:bg-gray-700 md:w-auto">
+				Telegram
 				<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
 					<path d="M5 12h14M12 5l7 7-7 7"></path>
 				</svg>
@@ -24,45 +24,13 @@
 </template>
 
 <script>
-	import axios from 'axios';
-
-	async function discord() {
-		return await axios
-			.get('https://discord.com/api/guilds/927945620554121306/widget.json')
-			.then(function (response) {
-				return {
-					name: response.data.name,
-					id: response.data.id,
-					invite: response.data.instant_invite,
-					members: response.data.members,
-					onlinecount: response.data.members.length,
-				};
-			})
-			.catch(function () {
-				return {
-					name: null,
-					id: null,
-					invite: null,
-					members: null,
-					onlinecount: null,
-				};
-			});
-	}
-
 	export default {
 		data() {
 			return {
 				github: 'https://github.com/moneymafia',
 				medium: 'https://moneymafia.medium.com/',
-				invite: 'https://discord.com/widget?id=927945620554121306&theme=light',
+				telegram: 'https://t.me/+U30ZfKzJl_VmZjll',
 			};
-		},
-		methods: {
-			async fetchdata() {
-				const response = await discord();
-
-				this.invite = response.invite;
-			},
 		},
 		async mounted() {
 			await this.fetchdata();
