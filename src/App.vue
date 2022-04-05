3768fb0af96e8a0b9dd8cf80884ad7ec5634bbc5
besoeasy <besoeasy@users.noreply.github.com>
2022-04-06 00:17:27 +0530

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
 	<div id="app">
-		<div class="container mx-auto flex flex-col flex-wrap items-center justify-between py-10 md:flex-row uppercase">
+		<div class="container mx-auto max-w-7xl flex flex-col flex-wrap items-center justify-between py-10 md:flex-row uppercase text-2xl font-bold text-gray-500">
 			<div class="relative flex flex-col md:flex-row">
-				<router-link to="/" class="flex items-center mb-5 font-medium text-gray-900 lg:w-auto lg:items-center lg:justify-center md:mb-0">
-					<span class="mx-auto text-xl font-black leading-none text-gray-900 select-none">MONEYMAFIA.ETH</span>
-				</router-link>
+				<router-link to="/" class="flex items-center lg:w-auto lg:items-center lg:justify-center"> MONEYMAFIA.ETH </router-link>
 			</div>
 
 			<div class="inline-flex items-center ml-5 space-x-6 lg:justify-end">
 				<nav class="flex flex-wrap items-center mb-5 text-base md:mb-0 md:pl-8 md:ml-8 md:border-l md:border-gray-200">
-					<router-link to="/portfolio" class="mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900">portfolio</router-link>
-					<router-link to="/service" class="mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900">SERVICE</router-link>
-					<router-link to="/team" class="mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900">TEAM</router-link>
-					<router-link to="/faq" class="mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900">FAQ</router-link>
-					<router-link to="/contact" class="mr-5 font-medium leading-6 text-gray-600 hover:text-gray-900">CONTACT</router-link>
+					<router-link to="/portfolio" class="mr-5">portfolio</router-link>
+					<router-link to="/service" class="mr-5">SERVICE</router-link>
+					<router-link to="/team" class="mr-5">TEAM</router-link>
+					<router-link to="/faq" class="mr-5">FAQ</router-link>
+					<router-link to="/contact" class="mr-5">CONTACT</router-link>
 				</nav>
 			</div>
 		</div>
@@ -22,7 +20,7 @@
 
 		<footer class="py-20">
 			<div class="container px-4 mx-auto">
-				<p class="text-center text-sm text-gray-400">Built with ❤️ In Dubai, UAE.</p>
+				<p class="text-center text-2xl text-gray-400">Built with ❤️ In Dubai, UAE.</p>
 			</div>
 		</footer>
 	</div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,12 @@
 	<div>
 		<div class="container mx-auto max-w-7xl px-5 py-20">
 			<div class="flex md:flex-row flex-col items-center">
-				<div class="md:w-1/2 p-5">
-					<p class="text-2xl font-bold uppercase text-gray-400">Money Mafia is a decenterlised organisation that <span class="text-white bg-gray-400">develops</span>, <span class="text-white bg-gray-400">manages</span> & <span class="text-white bg-gray-400">funds</span> blockchain startups.</p>
+				<div class="md:w-1/2">
+					<p class="uppercase text-3xl font-bold text-gray-500">Money Mafia is a decenterlised organisation that <span class="text-white bg-gray-400">develops</span>, <span class="text-white bg-gray-400">manages</span> & <span class="text-white bg-gray-400">funds</span> blockchain startups.</p>
 					<links />
 				</div>
 
-				<div class="md:w-1/2 p-5">
+				<div class="md:w-1/2">
 					<lottie-player class="object-cover object-center" autoplay="" loop="" mode="normal" src="img/main.json" background="transparent"></lottie-player>
 				</div>
 			</div>
