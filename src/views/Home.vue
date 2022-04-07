3a7e5f3a77ebd57bae2f1cee03ef2f1e82f38452
Money Mafia Manager <97112807+MoneyMafiaManager@users.noreply.github.com>
2022-04-07 21:38:14 +0530

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
 	<div>
 		<div class="container mx-auto max-w-5xl flex flex-col justify-between items-center relative font-mono">
 			<div class="flex flex-col px-6 py-40">
-				<p class="text-3xl my-6 text-center dark:text-white">MONEY MAFIA IS A DECENTERLISED ORGANISATION THAT DEVELOPS (🧑‍💻), MANAGES (⚙️) & FUNDS (💰) BLOCKCHAIN STARTUPS.</p>
+				<p class="text-3xl my-6 text-center dark:text-white">MONEY MAFIA IS A DECENTERLISED ORGANISATION THAT DEVELOPS, MANAGES & FUNDS BLOCKCHAIN STARTUPS.</p>
 				<div class="flex items-center justify-center mt-4">
 					<a href="https://github.com/moneymafia" class="uppercase py-2 my-2 px-4 md:mt-16 bg-transparent dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 border-2 border-gray-800 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white text-md"> GITHUB </a>
 
