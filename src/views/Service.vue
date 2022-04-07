a6f3d0a937fd0b1299fe859bb5554279b7ecbdd9
besoeasy <besoeasy@users.noreply.github.com>
2022-04-07 21:09:55 +0530

    pushed

--- src/views/Service.vue
+++ src/views/Service.vue
@@ -1,66 +1,65 @@
 <template>
-	<div class="">
-		<div class="py-10 container mx-auto max-w-7xl px-5">
-			<div class="flex md:flex-row flex-col items-center">
-				<div class="md:w-1/2 p-5">
-					<p class="text-4xl font-bold uppercase text-gray-400">We help <span class="text-white bg-gray-400">Ideas</span> that empower the blockchain technology</p>
-				</div>
-
-				<div class="md:w-1/2 p-5">
-					<lottie-player class="object-cover object-center" autoplay="" loop="" mode="normal" src="img/rocket.json" background="transparent"></lottie-player>
-				</div>
-			</div>
+	<div class="container mx-auto max-w-7xl px-5">
+		<div class="py-40">
+			<p class="text-3xl font-mono uppercase">We help <span class="text-white bg-red-300">IDEAS</span> that empower the blockchain technology.</p>
 		</div>
-		<div class="py-10 container mx-auto px-5">
+
+		<div class="py-10">
 			<section class="grid lg:grid-cols-3 grid-cols-1 gap-10 antialiased">
-				<article class="flex flex-col shadow-xl mx-auto bg-red-100 py-20 px-12 transform duration-500 hover:-translate-y-2 cursor-pointer max-h-190 rounded-md">
-					<h1 class="font-extrabold text-6xl mt-12 mb-10 text-gray-800">01.</h1>
-					<h2 class="font-bold mb-5 text-gray-800 text-2xl">DAPP Development</h2>
-					<p class="leading-relaxed text-gray-700">A decentralized application (dapp) is an application built on a decentralized network that combines a smart contract and a frontend user interface.</p>
-				</article>
+				<div v-for="(collapse, index) of collapses" :key="index">
+					<div class="p-1 shadow-xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-2xl">
+						<a class="block p-6 bg-white sm:p-8 rounded-xl">
+							<div class="mt-16 sm:pr-8">
+								<h5 class="text-xl font-bold text-gray-900">{{ collapse.title }}</h5>
 
-				<article class="flex flex-col shadow-xl mx-auto bg-yellow-100 py-20 px-12 transform duration-500 hover:-translate-y-2 cursor-pointer mt-0 max-h-190 rounded-md">
-					<h1 class="font-extrabold text-6xl mt-12 mb-10 text-gray-800">02.</h1>
-					<h2 class="font-bold mb-5 text-gray-800 text-2xl">Graph Protocol Development</h2>
-					<p class="leading-relaxed text-gray-700">The Graph is an indexing protocol for querying networks like Ethereum and IPFS.</p>
-				</article>
-
-				<article class="flex flex-col shadow-xl mx-auto bg-blue-100 py-20 px-12 transform duration-500 hover:-translate-y-2 cursor-pointer max-h-190 rounded-md">
-					<h1 class="font-extrabold text-6xl mt-12 mb-10 text-gray-800">03.</h1>
-					<h2 class="font-bold mb-5 text-gray-800 text-2xl">Smart Contract Development</h2>
-					<p class="leading-relaxed text-gray-700">A smart contract is a set of computer code that runs on the blockchain network.</p>
-				</article>
-
-				<article class="flex flex-col shadow-xl mx-auto bg-purple-100 py-20 px-12 transform duration-500 hover:-translate-y-2 cursor-pointer mt-0 max-h-190 rounded-md">
-					<h1 class="font-extrabold text-6xl mt-12 mb-10 text-gray-800">04.</h1>
-					<h2 class="font-bold mb-5 text-gray-800 text-2xl">Project Audit</h2>
-					<p class="leading-relaxed text-gray-700">A Project Audit is a formal review of a project, which is intended to assess the extent up to which project management standards are being upheld.</p>
-				</article>
-
-				<article class="flex flex-col shadow-xl mx-auto bg-red-100 py-20 px-12 transform duration-500 hover:-translate-y-2 cursor-pointer mt-0 max-h-190 rounded-md">
-					<h1 class="font-extrabold text-6xl mt-12 mb-10 text-gray-800">05.</h1>
-					<h2 class="font-bold mb-5 text-gray-800 text-2xl">Bot/Script Development</h2>
-					<p class="leading-relaxed text-gray-700">Bot development frameworks were created as advanced software tools that eliminate a large amount of manual work and accelerate the development process.</p>
-				</article>
-
-				<article class="flex flex-col shadow-xl mx-auto bg-yellow-100 py-20 px-12 transform duration-500 hover:-translate-y-2 cursor-pointer max-h-190 rounded-md">
-					<h1 class="font-extrabold text-6xl mt-12 mb-10 text-gray-800">06.</h1>
-					<h2 class="font-bold mb-5 text-gray-800 text-2xl">Web 3.0 Development</h2>
-					<p class="leading-relaxed text-gray-700">Web 3.0 is the third generation of Internet services that will focus on understanding and analyzing data to provide a semantic web. Its goal is to create more intelligent, connected, open, and user-friendly websites.</p>
-				</article>
-
-				<article class="flex flex-col shadow-xl mx-auto bg-blue-100 py-20 px-12 transform duration-500 hover:-translate-y-2 cursor-pointer mt-0 max-h-190 rounded-md">
-					<h1 class="font-extrabold text-6xl mt-12 mb-10 text-gray-800">07.</h1>
-					<h2 class="font-bold mb-5 text-gray-800 text-2xl">Blockchain Development</h2>
-					<p class="leading-relaxed text-gray-700">A blockchain is a decentralized digital ledger that saves transactions on thousands of computers around the globe. These are registered in a way that inhibits their subsequent modification.</p>
-				</article>
-
-				<article class="flex flex-col shadow-xl mx-auto bg-purple-100 py-20 px-12 transform duration-500 hover:-translate-y-2 cursor-pointer max-h-190 rounded-md">
-					<h1 class="font-extrabold text-6xl mt-12 mb-10 text-gray-800">08.</h1>
-					<h2 class="font-bold mb-5 text-gray-800 text-2xl">Hacking / RE</h2>
-					<p class="leading-relaxed text-gray-700">A security hacker is someone who explores methods for breaching defenses and exploiting weaknesses in a computer system or network.</p>
-				</article>
+								<p class="mt-2 text-sm text-gray-500">{{ collapse.text }}</p>
+							</div>
+						</a>
+					</div>
+				</div>
 			</section>
 		</div>
 	</div>
 </template>
+<script>
+	export default {
+		data() {
+			return {
+				collapses: [
+					{
+						title: 'DAPP Development',
+						text: 'A decentralized application (dapp) is an application built on a decentralized network that combines a smart contract and a frontend user interface.',
+					},
+					{
+						title: 'Blockchain Development',
+						text: 'A blockchain is a decentralized digital ledger that saves transactions on thousands of computers around the globe. These are registered in a way that inhibits their subsequent modification.',
+					},
+					{
+						title: 'Hacking / RE',
+						text: 'A security hacker is someone who explores methods for breaching defenses and exploiting weaknesses in a computer system or network.',
+					},
+					{
+						title: 'Web 3.0 Development',
+						text: 'Web 3.0 is the third generation of Internet services that will focus on understanding and analyzing data to provide a semantic web. Its goal is to create more intelligent, connected, open, and user-friendly websites.',
+					},
+					{
+						title: 'Bot/Script Development',
+						text: 'Bot development frameworks were created as advanced software tools that eliminate a large amount of manual work and accelerate the development process.',
+					},
+					{
+						title: 'Project Audit',
+						text: 'A Project Audit is a formal review of a project, which is intended to assess the extent up to which project management standards are being upheld.',
+					},
+					{
+						title: 'Smart Contract Development',
+						text: 'A smart contract is a set of computer code that runs on the blockchain network.',
+					},
+					{
+						title: 'Graph Protocol Development',
+						text: 'The Graph is an indexing protocol for querying networks like Ethereum and IPFS.',
+					},
+				],
+			};
+		},
+	};
+</script>
