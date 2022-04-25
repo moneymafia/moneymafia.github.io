d4be9c23793df18ef66fe78ef45030815ecf8f8f
besoeasy <besoeasy@users.noreply.github.com>
2022-04-25 22:53:49 +0530

    token

--- src/views/Dao.vue
+++ src/views/Dao.vue
@@ -7,6 +7,20 @@
 				as an organization represented by rules encoded as a transparent computer program, controlled by the organization members, and not influenced by a central government.
 			</p>
 		</div>
-		
+		<div class="py-10">
+			<aside class="p-12 pt-48 sm:p-16 sm:pt-64 bg-gradient-to-r from-purple-600 to-blue-500">
+				<div class="sm:items-end sm:justify-between sm:flex">
+					<div class="max-w-xl">
+						<p class="text-3xl font-bold text-white sm:text-4xl">PARTICIPATE IN GOVERNANCE USING $MAFIA TOKEN</p>
+					</div>
+
+					<a href="https://snapshot.org/#/moneymafia.eth" class="inline-block px-8 py-4 mt-4 text-blue-500 bg-white rounded-lg shadow-lg hover:opacity-75 sm:mt-0 sm:ml-8">
+						<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 sm:h-8 sm:w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+						</svg>
+					</a>
+				</div>
+			</aside>
+		</div>
 	</div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,13 +4,11 @@
 			<div class="flex flex-col px-6 py-40">
 				<p class="text-3xl my-6 text-center dark:text-white">MONEY MAFIA IS A DECENTERLISED ORGANISATION THAT DEVELOPS, MANAGES & FUNDS BLOCKCHAIN STARTUPS.</p>
 				<div class="flex items-center justify-center mt-4 uppercase">
-					<a href="https://github.com/moneymafia" class="py-2 my-2 px-5 md:mt-16 bg-transparent dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 border-2 border-gray-800 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white text-md"> GITHUB </a>
+					<a href="https://github.com/moneymafia" class="py-2 my-2 px-6 md:mt-16 bg-transparent dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 border-2 border-gray-800 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white"> GITHUB </a>
 
-					<a href="https://moneymafia.medium.com/" class="py-2 my-2 px-5 md:mt-16 bg-transparent dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 border-2 border-gray-800 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white text-md"> MEDIUM </a>
+					<a href="https://moneymafia.medium.com/" class="py-2 my-2 px-6 md:mt-16 bg-transparent dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 border-2 border-gray-800 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white"> MEDIUM </a>
 
-					<a href="https://t.me/+2wVpMgdCFethMjRl" class="py-2 my-2 px-5 md:mt-16 bg-transparent dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 border-2 border-gray-800 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white text-md"> TELEGRAM </a>
-
-					<a href="https://snapshot.org/#/moneymafia.eth" class="py-2 my-2 px-5 md:mt-16 bg-transparent dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 border-2 border-gray-800 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white text-md"> VOTE </a>
+					<a href="https://t.me/+2wVpMgdCFethMjRl" class="py-2 my-2 px-6 md:mt-16 bg-transparent dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 border-2 border-gray-800 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white"> TELEGRAM </a>
 				</div>
 			</div>
 		</div>
