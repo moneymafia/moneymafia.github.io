51f98e7a119a164b9da8e7f2554d94407b9459a5
besoeasy <besoeasy@users.noreply.github.com>
2022-07-15 06:23:00 +0530

    ---

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
 	<div>
-		<div class="container mx-auto max-w-5xl flex flex-col justify-between items-center relative font-mono">
-			<div class="flex flex-col px-6 py-40">
+		<div class="container mx-auto max-w-5xl flex flex-col justify-between items-center relative font-mono px-6 py-40">
+			<div class="flex flex-col">
 				<p class="text-3xl my-6 text-center dark:text-white">MONEY MAFIA IS A DECENTERLISED ORGANISATION THAT DEVELOPS, MANAGES & FUNDS BLOCKCHAIN STARTUPS.</p>
-				<div class="flex items-center justify-center mt-4 uppercase text-3xl">
+				<div class="flex items-center justify-center mt-4 uppercase md:text-3xl">
 					<a href="https://github.com/moneymafia" class="py-2 my-2 px-6 md:mt-16 bg-transparent dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 border-2 border-gray-800 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white"> GITHUB </a>
 					<a href="https://moneymafia.medium.com/" class="py-2 my-2 px-6 md:mt-16 bg-transparent dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 border-2 border-gray-800 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white"> MEDIUM </a>
 					<a href="https://t.me/+2wVpMgdCFethMjRl" class="py-2 my-2 px-6 md:mt-16 bg-transparent dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 border-2 border-gray-800 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white"> TELEGRAM </a>
