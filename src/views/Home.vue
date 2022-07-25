97753d7c2539d14bd508981db5194a73a87d7c44
Aman Singh Katal <besoeasy@users.noreply.github.com>
2022-07-25 22:20:00 +0530

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
 			<div class="flex items-center justify-center mt-4 uppercase md:text-2xl">
 				<a href="https://github.com/moneymafia" class="py-2 my-2 px-6 md:mt-16 bg-transparent dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 border-2 border-gray-800 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white"> GITHUB </a>
 				<a href="https://moneymafia.medium.com/" class="py-2 my-2 px-6 md:mt-16 bg-transparent dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 border-2 border-gray-800 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white"> MEDIUM </a>
-				<a href="https://t.me/+2wVpMgdCFethMjRl" class="py-2 my-2 px-6 md:mt-16 bg-transparent dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 border-2 border-gray-800 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white"> TELEGRAM </a>
+				<a href="https://t.me/moneymafiaeth" class="py-2 my-2 px-6 md:mt-16 bg-transparent dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 border-2 border-gray-800 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white"> TELEGRAM </a>
 			</div>
 		</div>
 	</div>
